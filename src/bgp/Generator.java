40712338a58871615e96392210dbf9e16d532325
Giulio Rossi <gioogle88@gmail.com>
2012-12-22 11:45:39 +0100

    Updated generator

--- src/bgp/Generator.java
+++ src/bgp/Generator.java
@@ -9,7 +9,7 @@ import java.util.Random;
 
 public class Generator {
 
-	static Random r = new Random(13); // or use Random(seed)
+	static Random r = new Random(); // or use Random(seed)
 
 	public static void main(String[] args) {
 		try {
@@ -28,33 +28,36 @@ public class Generator {
 			double time = 0, temp, uniform;
 			int intime;
 			int coin, type;
-			int count = 0, lambda = 1;
-			int prefixes_per_node = 10;
+			int count = 0, lambda = 5;
+			int stoptime = 5000;
+			int prefixes_per_node = 20;
+			int initalized_prefixesXnode_at_start = 4;
+			int numberofnodes = 2500;
 
-			int wd_prob = 48;
-			int new_pref_prob = wd_prob;
-			int old_prefix_prob = 1000 - (2 * wd_prob);
+			int wd_prob = 72;
+			int new_pref_prob = 928;
+			int old_prefix_prob = 1000 - wd_prob - new_pref_prob;
 
 			ArrayList<Table> topology = new ArrayList<Table>();
-			ArrayList<Table> new_prefixex = new ArrayList<Table>();
+			ArrayList<Table> new_prefixes = new ArrayList<Table>();
 
-			for (int i = 1; i <= 60; i++) {
-				for (int k = 0; k <= 4; k++)
-					topology.add(new Table(i + 60 * k, i, 1)); // topology.size()=60
-																// -->
-																// [0,59] --->
-																// prefixes from
-																// 1 to 60
+			for (int i = 1; i <= numberofnodes; i++) {
+				for (int k = 0; k < initalized_prefixesXnode_at_start; k++)
+					topology.add(new Table(i + numberofnodes * k, i, 1)); // topology.size()=60
+				// -->
+				// [0,59] --->
+				// prefixes from
+				// 1 to 60
 			}
-			count = 301;
+			count = topology.get(topology.size() - 1).getId();
 
 			do {
-				new_prefixex.add(new Table(count, 0, 0)); // new_prefixes.size()=600
+				new_prefixes.add(new Table(count, 0, 0)); // new_prefixes.size()=600
 															// --> [0,599] --->
 															// prefixes from 61
 															// to 660
 				count++;
-			} while (count <= 60 * prefixes_per_node);
+			} while (count <= numberofnodes * prefixes_per_node);
 
 			do {
 				uniform = r.nextDouble();
@@ -64,7 +67,7 @@ public class Generator {
 				coin = (r.nextInt(1000)) + 1; // uniform random number [1,1000]
 												// for event_type generator
 				if (coin <= wd_prob) {
-					type = 1; // withdrawal with probability 0,048
+					type = 1; // withdrawal with probability 0,072
 					int position = r.nextInt(topology.size());
 					String message = type + "\t"
 							+ topology.get(position).getId() + "\t"
@@ -74,7 +77,7 @@ public class Generator {
 					Table a = topology.get(position);
 					a.setState(0);
 					topology.set(position, a);
-					new_prefixex.add(a);
+					new_prefixes.add(a);
 					topology.remove(position);
 
 					bufferevent.write(message);
@@ -83,30 +86,21 @@ public class Generator {
 					bufferevent2.flush();
 
 					// time = time + temp;
-				} else if (wd_prob < coin
-						&& coin <= new_pref_prob + old_prefix_prob) {
-					type = 3; // old prefix update with probability 0,904
-					int position = r.nextInt(topology.size());
-					String message = "2\t" + topology.get(position).getId()
-							+ "\t" + topology.get(position).getNode() + "\t"
-							+ intime + "\n";
-
-					bufferevent.write(message);
-					bufferevent.flush();
-				} else {
-					type = 2; // new prefix announcement with probability 0,048
-					int position = r.nextInt(new_prefixex.size());
-					int random_node = topology.get(r.nextInt(topology.size())).getNode();
+				} else if (wd_prob < coin && coin <= new_pref_prob + wd_prob) {
+					type = 2; // new prefix announcement with probability 0,928
+					int position = r.nextInt(new_prefixes.size());
+					int random_node = topology.get(r.nextInt(topology.size()))
+							.getNode();
 					String message = type + "\t"
-							+ new_prefixex.get(position).getId() + "\t"
+							+ new_prefixes.get(position).getId() + "\t"
 							+ random_node + "\t" + intime + "\n";
 
-					Table a = new_prefixex.get(position);
+					Table a = new_prefixes.get(position);
 					a.setState(1);
 					a.setNode(random_node);
-					new_prefixex.set(position, a);
+					new_prefixes.set(position, a);
 					topology.add(a);
-					new_prefixex.remove(position);
+					new_prefixes.remove(position);
 
 					bufferevent.write(message);
 					bufferevent.flush();
@@ -114,8 +108,17 @@ public class Generator {
 					bufferevent2.flush();
 
 					// time = time + temp;
+				} else {
+					type = 3; // old prefix update with probability 0,904
+					int position = r.nextInt(topology.size());
+					String message = "2\t" + topology.get(position).getId()
+							+ "\t" + topology.get(position).getNode() + "\t"
+							+ intime + "---------------\n";
+
+					bufferevent.write(message);
+					bufferevent.flush();
 				}
-			} while (time < 500);
+			} while (time < stoptime);
 			bufferevent.close();
 			bufferevent2.close();
 		} catch (IOException e) {
